d0c1529173413b04d9a168e3d58b35bcdf66889f
Takehiro_Suzuki <t.suzuki.ug@azbil.com>
2018-02-19 10:23:56 +0900

    implemented mahalanobis

--- jit_model/neighbor.py
+++ jit_model/neighbor.py
@@ -2,6 +2,7 @@
 import numpy as np
 from sklearn.linear_model import ElasticNet
 from scipy.spatial.distance import mahalanobis
+from .weight import weight_func
 
 class BaseNeighborSearch():
     def __init__(self):
@@ -38,9 +39,23 @@ class EuclideanSearch(BaseNeighborSearch):
         pass
 
 class MahalanobisSearch(BaseNeighborSearch):
-    def __init__(self):
+    def __init__(self,k=10):
         super().__init__()
+        self.k = k
 
     def search(self,query,X):
-        mahalanobis(query,X,)
+        # precision matrix
+        vi = np.linalg.inv(np.cov(X.T))
+        dist = []
+        for idx,x in enumerate(X):
+            mdist = mahalanobis(query,x,vi)
+            dict_ = dict(index=idx,distance=mdist)
+            dist.append(dict_)
+        # sort & limit
+        dist_s = sorted(dist,key=lambda x: x["distance"])[:self.k]
+        local_indices = [ds["index"] for ds in dist_s]
+        X_local = X[local_indices,:]
+        local_distance = [ds["distance"] for ds in dist_s]
+        weight = weight_func(query,X_local,local_distance)
+
         return X_local,weight,local_indices
---
+++ jit_model/weight.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+
+def weight_func(query,X_local,dist):
+    h = determine_bandwidth(query,X_local)
+    x_tri = dist/h
+    weight = tricube(x_tri)
+    return weight
+
+def tricube(x):
+    weight = np.zeros(x.shape[0])
+    for i,xx in enumerate(x):
+        weight[i] = (1 - np.linalg.norm(xx)**3)**3
+    weight[weight < 0] = 0
+    return weight
+
+def determine_bandwidth(query,X_local):
+    half_width = np.linalg.norm(X_local[-1] - query)
+    return half_width * 2
